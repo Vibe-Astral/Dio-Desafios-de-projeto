2d75c5f93e632ceb3e86e0b31f68c6537fdb8496
jorge antonio <jorgeantonio.acll@gmail.com>
2022-06-27 11:32:21 -0300

    TWEET

---
+++ Dio Desafio Js/desafio-3-TWEET/TWEET.js
@@ -0,0 +1,8 @@
+let T = ("fhaisjfijasfjoasf0asfj90asf23423423423432423434234223423423423gx\sgsgegfgfgdggffdfdsfsdfssssssssssssssssssssssssssssssssssss614446464646446j9jas9fj9asjf9ja9sfj9jaf9j9asfj9ajf0a9sfjjas0fja90sfj9ajsf9ajsf9jas9f")
+console.log(T.length)
+
+if((T.length <= 140) && (T.length >= 1)){
+  console.log('TWEET');
+}else{
+  console.log('MUTE');
+}
